86ea195ba72b006be8315b2a791ac96783dc9ac6
Julius Härtl <jus@bitgrid.net>
2019-03-14 14:42:42 +0100

    Dispatch event to allow apps to load their script for collections
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/files_sharing/appinfo/app.php
+++ apps/files_sharing/appinfo/app.php
@@ -45,6 +45,9 @@ $eventDispatcher->addListener(
 		\OCP\Util::addScript('files_sharing', 'dist/additionalScripts');
 	}
 );
+\OC::$server->getEventDispatcher()->addListener('\OCP\Collaboration::loadAdditionalScripts', function () {
+	\OCP\Util::addScript('files_sharing', 'dist/collaboration');
+});
 
 $config = \OC::$server->getConfig();
 $shareManager = \OC::$server->getShareManager();
